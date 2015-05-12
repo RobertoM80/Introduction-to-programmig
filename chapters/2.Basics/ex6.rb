5d369a98c6a18925f06fdc9cf7b9dc21924c2375
Roberto Mirabella <bronzetto@outlook.com>
2015-05-12 17:19:58 +0100

    Added ex n. 6

---
+++ chapters/2.Basics/ex6.rb
@@ -0,0 +1,8 @@
+#Write a program that calculates the squares of 
+#3 float numbers of your choosing and outputs the result to the screen.
+
+numbers = [22.5, 6.4, 12.1]
+
+puts numbers[0] * numbers[0]
+puts numbers[1] * numbers[1]
+puts numbers[2] * numbers[2]
