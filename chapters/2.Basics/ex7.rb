cb43e93d80fcaa0e603fca9976ee563455c6e916
Roberto Mirabella <bronzetto@outlook.com>
2015-05-12 17:25:26 +0100

    Added ex n. 7

---
+++ chapters/2.Basics/ex7.rb
@@ -0,0 +1,8 @@
+#What does the following error message tell you?
+
+  #SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+    #from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+
+
+#It tells me that on line 16 the program found a ) instead a }. 
+#Probably we forgot to close a hash properly.
