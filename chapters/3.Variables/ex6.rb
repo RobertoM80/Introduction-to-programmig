934f14a4c6d9eed34101cb7c2baf69b65c2d937f
Roberto Mirabella <bronzetto@outlook.com>
2015-05-14 17:34:47 +0100

    Added ex6.rb

---
+++ chapters/3.Variables/ex6.rb
@@ -0,0 +1,12 @@
+=begin
+What does the following error message tell you?
+
+NameError: undefined local variable or method `shoes' for main:Object
+  from (irb):3
+  from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+
+
+ANSWER:
+
+Tells me that using irb we were trying to access a variable wich was not set as a global 
+variable
