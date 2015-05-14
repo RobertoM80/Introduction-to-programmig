d045c596b7cebd727c710ebe454468dbdebb86a9
Roberto Mirabella <bronzetto@outlook.com>
2015-05-14 18:41:51 +0100

    Added ex6.rb to Methods Chapter

---
+++ chapters/4.Methods/ex6.rb
@@ -0,0 +1,15 @@
+=begin
+  
+What does the following error message tell you?
+
+ArgumentError: wrong number of arguments (1 for 2)
+  from (irb):1:in `calculate_product'
+  from (irb):4
+  from /Users/username/.rvm/rubies/ruby-2.0.0-p353/bin/irb:12:in `<main>'
+
+ANSWER:
+
+It tells me that we create a method called calculate_product wich takes two
+arguments but we just put one.
+  
+end
