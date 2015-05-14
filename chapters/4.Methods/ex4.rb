511042a0fecf0c8c93b1180a0b7d00a308ceeff1
Roberto Mirabella <bronzetto@outlook.com>
2015-05-14 18:31:35 +0100

    Added ex4.rb to Methods Chapter

---
+++ chapters/4.Methods/ex4.rb
@@ -0,0 +1,23 @@
+=begin
+  
+What will the following code print to the screen?
+
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")
+
+
+
+
+ANSWER:
+
+Nothing is execute after the return, so nothing is printed
+  
+=end
+
+
+
