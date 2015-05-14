623840e5db88a7a3855a64038de058461dfa53ff
Roberto Mirabella <robertomirabella@Robertos-MacBook-Pro.local>
2015-05-14 13:38:17 +0100

    Added age.rb file

---
+++ chapters/3.Variables/age.rb
@@ -0,0 +1,11 @@
+#Write a program called age.rb that asks a user how old they are and then tells them 
+#how old they will be in 10, 20, 30 and 40 years. Below is the output for someone 20 years old. 
+
+
+puts "Tell how old are you and I will tell you how old you will be in 10, 20, 30 and 40 years:"
+how_old = gets.chomp.to_i
+
+puts "in 10 years you will be #{how_old + 10}"
+puts "in 20 years you will be #{how_old + 20}"
+puts "in 30 years you will be #{how_old + 30}"
+puts "in 40 years you will be #{how_old + 40}"
