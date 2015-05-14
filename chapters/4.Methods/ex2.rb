84e09a852a45419e7e90518ed60912c43b3f619a
Roberto Mirabella <bronzetto@outlook.com>
2015-05-14 18:00:49 +0100

    Added ex2.rb to Method Chapter

---
+++ chapters/4.Methods/ex2.rb
@@ -0,0 +1,24 @@
+=begin
+  
+What do the following expressions evaluate to?
+
+1. x = 2
+
+2. puts x = 2
+
+3. p name = "Joe"
+
+4. four = "four"
+
+5. print something = "nothing"
+
+
+ANSWER:
+
+1. 2
+2. nil
+3. "Joe"
+4. four    (wrong it was "four")
+5. nil
+  
+end
