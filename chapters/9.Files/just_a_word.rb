774ec45793c81ff27e7b931460cea67018a77969
Roberto Mirabella <bronzetto@outlook.com>
2015-05-17 15:54:01 +0100

    Doubts about File

---
+++ chapters/9.Files/just_a_word.rb
@@ -0,0 +1,11 @@
+#
+#
+# Ok, I have made the exercises in irb but I would say I copy 
+#following the book. File section turns out to be more confusing
+#than the rest. Is this something I will resolve going ahead or
+#I should be perfectly familiar already??
+#
+#
+#
+#
+#
