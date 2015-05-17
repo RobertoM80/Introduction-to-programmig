38c0cddf1ecbe17e0696210019556302ff94e5b5
Roberto Mirabella <bronzetto@outlook.com>
2015-05-17 13:32:08 +0100

    Added ex7.rb to Hashes Chapter

---
+++ chapters/8.Hash/ex7.rb
@@ -0,0 +1,10 @@
+#Given the following code...
+
+x = "hi there"
+my_hash = {x: "some value"}
+my_hash2 = {x => "some value"}
+
+#What's the difference between the two hashes that were created?
+
+#They are both ashes but the first has a symbol as a key and the
+#second one has the value of a variable named x.
