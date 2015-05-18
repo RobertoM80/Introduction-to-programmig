3a9d934571161ce11e189aa881557dfd308778fc
Roberto Mirabella <bronzetto@outlook.com>
2015-05-18 12:15:44 +0100

    Added ex2.rb to More_stuff Chapter

---
+++ chapters/10.More_stuff/ex2.rb
@@ -0,0 +1,9 @@
+#What will the following program print to the screen? What will it return?
+
+def execute(&block)
+  block.call
+end
+
+execute { puts "Hello from inside the execute method!" }
+
+#It will print "Hello...!" and will return nil
