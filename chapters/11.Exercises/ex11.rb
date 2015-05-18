24f367348bf3adffda04136c9958cac0e816498a
Roberto Mirabella <bronzetto@outlook.com>
2015-05-18 14:39:23 +0100

    Added ex11.rb to Exercises Chapter

---
+++ chapters/11.Exercises/ex11.rb
@@ -0,0 +1,8 @@
+#Look at several Rails/Ruby online API sources and say which one you like best and why.
+
+#I like more our ruby-doc.org because is schematic and is easy to move around the documentation
+#or at least that is my opinion for now. 
+#
+#
+#
+#
