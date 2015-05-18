fa9381a5d8fd994fc87e2cdfb2ca2f419f8e8414
Roberto Mirabella <bronzetto@outlook.com>
2015-05-18 13:26:14 +0100

    Added ex7.rb to Exercises Chapter

---
+++ chapters/11.Exercises/ex7.rb
@@ -0,0 +1,11 @@
+#What's the major difference between an Array and a Hash?
+
+#
+#The major gifference is that arrays have as index numbers and the hashes 
+#can have different index values such as symbols, numbers or strings and 
+#they are called keys.
+#
+#
+#
+#
+#
