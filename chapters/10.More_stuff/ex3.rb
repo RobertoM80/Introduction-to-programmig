3caea48fc1a88aa450c13bba8ac2f830d1041540
Roberto Mirabella <bronzetto@outlook.com>
2015-05-18 12:23:44 +0100

    Added ex3.rb to More_stuff Chapter

---
+++ chapters/10.More_stuff/ex3.rb
@@ -0,0 +1,12 @@
+=begin 
+What is exception handling and what problem does it solve?
+
+ANSWER:
+
+Exception handlin is a specific process to skip errors in the program without terminate the 
+program to a half way. We can predict how the program flow will be in case some error
+usually from outside our program, will impact our script.
+We use for this the words : begin, rescue, end.
+
+
+=end 
