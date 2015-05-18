d14335d39d525563bb3f19bc08819a3c6661cfa0
Roberto Mirabella <bronzetto@outlook.com>
2015-05-18 12:28:44 +0100

    Changed text

--- chapters/10.More_stuff/ex2.rb
+++ chapters/10.More_stuff/ex2.rb
@@ -1,9 +1,9 @@
 #What will the following program print to the screen? What will it return?
 
 def execute(&block)
-  block.call
+  block
 end
 
 execute { puts "Hello from inside the execute method!" }
 
-#It will print "Hello...!" and will return nil
+#It will print "Hello...!" and will return nil =====> wrong! missing the .call
