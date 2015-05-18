b09779541fac6a72124bd26fcf3555eec21c492c
Roberto Mirabella <bronzetto@outlook.com>
2015-05-18 13:51:17 +0100

    Added ex8.rb to Exercises Chapter

---
+++ chapters/11.Exercises/ex8.rb
@@ -0,0 +1,15 @@
+#Create a Hash using both Ruby syntax styles.
+
+my_animals = {:cat => "felix",
+              :dog => "Birillo",    #old style hash
+              :bird => "Gino"
+             }
+
+
+my_cars = {alfa: "red",
+           talbot: "grey",
+           wrangler: "yellow"}
+
+
+p my_animals
+p my_cars
